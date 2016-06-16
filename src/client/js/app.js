87d81684af195aea7afe9f713e62916ad5189436
Dan Hart <dan@danhart.co.uk>
2016-06-16 12:06:08 +0100

    Update build process for client-side JS

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, Link } from 'react-router'
-import routes from '../../routes.jsx';
+import routes from '../../routes';
 
 render((
   <Router>
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 require('babel-register');
 const app = require('./app');
 
-var server = app.listen(0);
-console.log("server started");
+var server = app.listen(0, function() {
+  console.log("Server running on http://localhost:" + server.address().port);
+});
 
 module.exports = server;
